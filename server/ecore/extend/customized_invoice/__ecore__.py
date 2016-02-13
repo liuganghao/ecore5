51f0950880c7651ee57237e59681b64913ccff80
Diego <diego@avalos.co>
2016-02-13 13:45:24 -0500

    emessenger

--- server/ecore/extend/customized_invoice/__ecore__.py
+++ server/ecore/extend/customized_invoice/__ecore__.py
@@ -3,26 +3,21 @@
     'name': "Accounting -> Multiple Professional & Customizable Invoice Templates",
 
     'summary': """
-        Print professional and good looking Invoices reports that you can customize with high quality logos and beautiful unlimited brand colors. Numerous Invoice templates and colors to choose from """,
+        Print professional and good looking Invoices reports that you can customize with high quality logos and beautiful unlimited brand colors.""",
 
     'description': """
-        This module will install a customized client invoice report for accounting module.You will be able to customize the invoice colors,logo and the style/format of invoice to look professional and appealing to your customers. You can also create your own template from scratch or edit one of the existing templates that come with this module 
+        This module will install a customized client invoice report for accounting module.You will be able to customize the invoice colors,
+        logo and the style/format of invoice to look professional and appealing to your customers. You can also create your own template 
+        from scratch or edit one of the existing templates that come with this module 
     """,
     'images': ['static/description/invoice.png'],
-    'price': 29,
-    'currency': 'EUR',
 
 
     'author': "Optima ICT Services LTD",
     'website': "http://www.optima.co.ke",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/ecore/ecore/blob/master/ecore/addons/base/module/module_data.xml
-    # for the full list
     'category': 'Accounting & Finance',
     'version': '0.1',
-
-    # any module necessary for this one to work correctly
     'depends': ['base', 'account', 'optima_social'],
 
     # always loaded
